684764f4a1dbf1f3ea5e8a5a425de73904fa143c
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2025-12-01 14:12:07 +0530

    Create Array Search.java

---
+++ GFG/GFG BASIC/Array Search.java
@@ -0,0 +1,24 @@
+// Given an array, arr[] of n integers, and an integer element x, find whether element x is present in the array.
+//Return the index of the first occurrence of x in the array, or -1 if it doesn't exist.
+
+// Examples:
+
+// Input: arr[] = [1, 2, 3, 4], x = 3
+// Output: 2
+// Explanation: For array [1, 2, 3, 4], the element to be searched is 3. Since 3 is present at index 2, the output is 2.
+
+
+class Solution {
+    public int search(int arr[], int x) {
+        // code here
+        int result=-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==x){
+                result =i;
+                break;
+            }
+        }
+        
+        return result;
+    }
+}
