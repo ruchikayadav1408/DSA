536bb4bba311282150a0011f41d47449ee5d9482
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2025-12-22 14:43:29 +0530

    Create Print Elements of Array.java

---
+++ GFG/GFG BASIC/Print Elements of Array.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+//Input: arr[] = [1, 2, 3, 4, 5]
+//Output: 1 2 3 4 5
+class Solution {
+    // Just print the space seperated array elements
+    void printArray(int arr[]) {
+        //   code here
+        for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]+" ");
+        }
+    }
+}
