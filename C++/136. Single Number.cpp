278703519e68e09a8e8a6fa81a6044e76abaaf3a
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2026-01-22 17:30:40 +0530

    Create 136. Single Number.cpp

---
+++ C++/136. Single Number.cpp
@@ -0,0 +1,24 @@
+// Example 1:
+
+// Input: nums = [2,2,1]
+
+// Output: 1
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+     for(int i=0;i<nums.size();i++){
+             int count=0;
+            for(int j=0;j<nums.size();j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+            if(count==1){
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+};
