6149e250e81bcb9adf598963afb4d2b873907dd0
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2026-02-04 21:55:21 +0530

    Create change array value - backtracking.java

---
+++ PRACTICE/Arrays/change array value - backtracking.java
@@ -0,0 +1,33 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code 
+//change array value - backtracking
+
+
+class Main {
+    
+    
+    public static void changearr(int arr[], int i, int val){
+        if(i==arr.length){
+            printarr(arr);
+            return;
+        }
+        arr[i]= val;
+        changearr(arr,i+1, val+1);
+        arr[i]=arr[i]-1;
+        
+        
+    }
+    
+    public static void printarr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+   
+    public static void main(String[] args) {
+       int arr[]=new int[5];
+       changearr(arr, 0, 1);
+       printarr(arr);
+    }
+}
