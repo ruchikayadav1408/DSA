2463b87db29a276feaf85c2368af061a00fc6540
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2026-03-02 21:23:53 +0530

    Create Next Greater Element.java

---
+++ GFG/MEDIUM/Next Greater Element.java
@@ -0,0 +1,36 @@
+class Solution {
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        // code here
+         Stack<Integer> s= new Stack<>();
+
+    ArrayList<Integer> al= new ArrayList<>();
+
+     int nextgreater[]= new int[arr.length];
+     for(int i=arr.length-1;i>=0;i--){
+
+        while(!s.isEmpty() && arr[s.peek()]<=arr[i]){
+            s.pop();
+        }
+
+
+        if(s.isEmpty()){
+         nextgreater[i]=-1;
+        }else{
+            nextgreater[i]=arr[s.peek()];
+        }
+
+        s.push(i);
+     }   
+     
+     
+     ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int a : nextgreater) {
+            list.add(a);
+        }
+        
+        return list;
+    
+    
+    }
+}
